4e707d6f616b136f4cbe15097af4e0a596f2029b
trieunb <trieunb08@gmail.com>
2018-04-25 22:01:29 +0700

    update code

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="col-md-3 left_col menu_fixed">
   <div class="left_col scroll-view">
     <div class="navbar nav_title" style="border: 0;">
-      <a href="/dashboard" class="site_title"><i class="fa fa-paw"></i> <span>Dev!</span></a>
+      <a href="/admin/dashboard" class="site_title"><i class="fa fa-paw"></i> <span>Dev!</span></a>
     </div>
     <div class="clearfix"></div>
     <!-- /menu profile quick info -->
